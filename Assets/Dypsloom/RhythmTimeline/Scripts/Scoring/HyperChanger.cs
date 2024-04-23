4b4e0f0a4fc6f2fda83ee4e4e15c29bbed1f4816
JoaoVBonifácio <jboni2014@gmail.com>
2024-04-22 22:43:50 -0300

    Elliot Show Time
    v0.0.3

--- Assets/Dypsloom/RhythmTimeline/Scripts/Scoring/HyperChanger.cs
+++ Assets/Dypsloom/RhythmTimeline/Scripts/Scoring/HyperChanger.cs
@@ -9,14 +9,18 @@ namespace Dypsloom.RhythmTimeline.UI
     {
         [Tooltip("The slider.")]
         [SerializeField] protected Slider m_Slider;
+        [SerializeField] protected Slider m_Slider2;
         [Tooltip("The image slider fill (optional).")]
         [SerializeField] protected Image m_SliderFill;
+        [SerializeField] protected Image m_SliderFill2;
         [Tooltip("The Slider Gradient.")]
         [SerializeField] protected Gradient m_SliderGradient;
+        [SerializeField] protected Gradient m_SliderGradient2;
 
-        public Image imageBar, Background;
+        //public RectTransform newFillRect;
 
-        public Sprite newImageBar, newBackground; // A nova imagem que você deseja usar
+        public GameObject hyperPanel;
+
 
         public void SetRank(float p, ScoreRank rank)
         {
@@ -36,8 +40,33 @@ namespace Dypsloom.RhythmTimeline.UI
                 Debug.Log("Slider atingiu 100%!");
                 // Faça o que for necessário quando o slider atingir 100%
 
-                imageBar.sprite = newImageBar;
-                Background.sprite = newBackground;
+                m_Slider2.gameObject.SetActive(true);
+
+                if (m_Slider2 != null && m_Slider2.IsActive())
+                {
+                    m_Slider2.value = p;
+                }
+
+                if (m_SliderFill2 != null && m_SliderGradient2 != null)
+                {
+                    m_SliderFill2.color = m_SliderGradient2.Evaluate(p / 100f);
+                }
+
+                if (m_Slider2.value == m_Slider2.maxValue)
+                {
+                    hyperPanel.SetActive(true);
+                } else
+                {
+                    hyperPanel.SetActive(false);
+                }
+
+                
+
+            }
+
+            if(m_Slider.value == m_Slider.minValue)
+            {
+                m_Slider2.gameObject.SetActive(false);
             }
 
         }
